afe3fd2e02483ce947abf856c131d2537d1039d1
ras-blue <binmanchang24@gmail.com>
2024-08-10 08:58:03 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/hire_me_web_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hire_me_web_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:hire_me_web_admin/views/screens/main_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: kIsWeb || Platform.isAndroid
+          ? FirebaseOptions(
+              apiKey: "AIzaSyDQKk13dUQUTngRM9xhe6_--fP6pMYEn38",
+              appId: "1:66984640335:web:89b3077aca21d9fea94f3a",
+              messagingSenderId: "66984640335",
+              projectId: "hire-me-da0f9",
+              storageBucket: "hire-me-da0f9.appspot.com",
+            )
+          : null);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MainScreen(),
+      builder: EasyLoading.init(),
+    );
+  }
+}
